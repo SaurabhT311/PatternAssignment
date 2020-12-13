232301751c889ef941c0fa22fbb54c85007139d9
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-13 20:53:00 +0530

    Added code for the password with minimum length 8

--- pattern.sh
+++ pattern.sh
@@ -1,2 +1,43 @@
-echo "Welcome"
-s
+#!/bin/bash -x
+
+echo "User Registration"
+
+read -p "Enter the First name: " name
+read -p "Enter the Last name: " lastname
+read -p "enter User Email ID: " emailid
+read -p "enter the  mobile no.: " phoneno
+read -p "enter the password with minimum length 8: " pass
+
+pattern="^[A-Z][a-zA-Z]{2,}"
+pattern1="^[A-Z][a-zA-Z]{2,}"
+emailpattern="^[a-zA-z]+([._][a-z0-9]+)?@[a-z]+.[a-z]+([.][a-z]+)$"
+mobilepattern=^[0-9][1-9]" "[0-9]{10}
+minimumpasswordlength=[\$.#@a-zA-Z0-9]{8}[a-zA-Z]*
+
+if [[ $name =~ $pattern && $lastname =~ $pattern1 ]]
+then
+	echo "$name" "$lastname"
+else
+	echo "please enter valid name"
+fi
+
+if [[ $emailid =~ $emailpattern ]]
+then
+	echo "$emailid"
+else
+	echo "enter valid Email-ID"
+fi
+
+if [[ $phoneno =~ $mobilepattern ]]
+then
+	echo "+$phoneno"
+else
+	echo "please enter valid phone number with correct country code"
+fi
+
+if [[ $pass =~ $minimumpasswordlength ]]
+then
+	echo "password is: " $pass*****
+else
+	echo "Please enter valid password"
+fi

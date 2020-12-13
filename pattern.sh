94786064714384426c903708fe33b7f5c1752771
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-13 18:27:13 +0530

    Added the code for lastname starting with captial letter and minimum of 3 alphabet

--- pattern.sh
+++ pattern.sh
@@ -1,2 +1,13 @@
-echo "Welcome"
-s
+#!/bin/bash -x
+
+read -p "Enter the First name: " name
+read -p "Enter the Last name: " lastname
+pattern="^[A-Z][a-zA-Z]{2,}"
+pattern1="^[A-Z][a-zA-Z]{2,}"
+
+if [[ $name =~ $pattern && $lastname =~ $pattern1 ]]
+then
+	echo "$name" "$lastname"
+else
+	echo "please enter valid name"
+fi

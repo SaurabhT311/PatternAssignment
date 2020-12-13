5364ab3e696ca16463b6a253256df00fc43021d2
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-13 19:12:10 +0530

    Added code to check valid emailId E.g.- abc.xyz@bl.co.in

--- pattern.sh
+++ pattern.sh
@@ -1,2 +1,25 @@
-echo "Welcome"
-s
+#!/bin/bash -x
+
+echo "User Registration"
+
+read -p "Enter the First name: " name
+read -p "Enter the Last name: " lastname
+read -p "enter User Email ID: " emailid
+
+pattern="^[A-Z][a-zA-Z]{2,}"
+pattern1="^[A-Z][a-zA-Z]{2,}"
+emailpattern="^[a-zA-z]+([._][a-z0-9]+)?@[a-z]+.[a-z]+([.][a-z]+)$"
+
+if [[ $name =~ $pattern && $lastname =~ $pattern1 ]]
+then
+	echo "$name" "$lastname"
+else
+	echo "please enter valid name"
+fi
+
+if [[ $emailid =~ $emailpattern ]]
+then
+	echo "$emailid"
+else
+	echo "enter valid Email-ID"
+fi

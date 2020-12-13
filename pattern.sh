2bb732f2324ce982ce1e39ef54b0e33a15f307b0
SaurabhT311 <tripathisaurabh.311@gmail.com>
2020-12-13 18:16:00 +0530

    Added the code for entering valid first name in captial letter and minimum 3 alphabet

--- pattern.sh
+++ pattern.sh
@@ -1,2 +1,11 @@
-echo "Welcome"
-s
+#!/bin/bash -x
+
+read -p "Enter the First name: " name
+pattern="^[A-Z][a-zA-Z]{2,}"
+
+if [[ $name =~ $pattern ]]
+then
+	echo "$name"
+else
+	echo "invalid name"
+fi
